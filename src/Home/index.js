cc7fed11407a5f90dd4cf0be13d8ab7c7723c9bc
Rafael-Benicio <jerobenicio@gmail.com>
2021-12-21 15:45:11 -0300

    Pre-programing pronto

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import styles from "./styles"
+
 export default function App() {
   return (
     <View >
---
+++ src/Home/styles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {StyleSheet, Dimensions} from 'react-native'
+
+const widthWi = Dimensions.get('window').width;
+const heightHe = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+	
+})
+
+export default styles
