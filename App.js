e279323aa3bf424cabe1bed36fbb9484859ea31b
Rafael-Benicio <jerobenicio@gmail.com>
2021-12-21 15:34:35 -0300

    Navigation

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import Home from './src/Home'
+import List from './src/List'
 
 export default function App() {
   return (
-    <View >
-      <Text>Hello World!</Text>
-    </View>
+    <NavigationContainer>
+           <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="List" component={List}/>
+           </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+}    
 
