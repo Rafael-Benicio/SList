5ffa1f4c75e86bf23ce29a3cdd57315b67c401da
Rafael-Benicio <jerobenicio@gmail.com>
2021-12-27 15:00:40 -0300

    estrutura da pagina Home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,TouchableOpacity,TouchableHighlight ,Image} from 'react-native';
 
 import styles from "./styles"
 
 export default function App() {
   return (
-    <View >
-      <Text>Home!</Text>
+    <View style={styles.background}>
+      {/*Cabeçalho*/}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>SList</Text>
+      </View>
+      {/*Lista*/}
+      <View style={styles.listItem}>
+        {/*Itens com listas*/}
+        <View style={styles.itemList}>
+            <TouchableOpacity activeOpacity={0.8}>
+                <Image style={styles.itemImg} source={require('./../../assets/icon.png')}/>
+            </TouchableOpacity>
+            <Text style={styles.itemText}>Nome 1</Text>
+        </View>
+        {/*Buttão de adicinar à lista*/}
+        <TouchableOpacity activeOpacity={0.95} style={styles.addButton}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -5,7 +5,54 @@ const widthWi = Dimensions.get('window').width;
 const heightHe = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-	
+	background:{
+		backgroundColor:'#a0f',
+		flex:1
+	},
+	header:{
+		backgroundColor:'#b6f',
+		height:heightHe*0.11,
+		justifyContent:'center',
+		paddingHorizontal:widthWi*0.05
+	},
+	headerText:{
+		fontSize:30,
+		fontWeight:'bold',
+		color:'#fff'
+	},
+	listItem:{
+		paddingVertical: heightHe*0.02,
+		alignItems:'center'
+	},
+	itemList:{
+		width:widthWi*0.9,
+		height:heightHe*0.17,
+		marginBottom:heightHe*0.02,
+		backgroundColor:'#000',
+		borderRadius:8,
+	},
+	itemImg:{
+		width:widthWi*0.9,
+		height:heightHe*0.17,
+		borderRadius:8,
+		opacity:0.9
+		// resizeMode: 'stretch',
+	},
+	itemText:{
+		position:'absolute',
+		bottom:0,
+		fontSize:20,
+		fontWeight:'bold',
+		marginLeft:widthWi*0.02
+	},
+	addButton:{
+		backgroundColor:'#0f0',
+		height: widthWi*0.14,
+		width: widthWi*0.14,
+		borderRadius:8,
+		justifyContent:'center',
+		alignItems:'center'
+	},
 })
 
 export default styles
