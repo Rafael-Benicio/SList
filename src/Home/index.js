9d61596b74c16f19f35829b6a5d9484c46043d5e
Rafael-Benicio <jerobenicio@gmail.com>
2022-01-11 01:20:20 -0300

    Add element window

--- src/Home/index.js
+++ src/Home/index.js
@@ -5,12 +5,14 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import styles from "./styles"
 
 export default function App() {
-  const [itemLista,setItemLista]=useState([{name:'Nome1',image:require('./../../assets/icon.png'),imageSet:1}])
+  const [itemLista,setItemLista]=useState([])
   const [imageSet,setImageSet]=useState(['cover', 'contain', 'stretch', 'repeat', 'center'])
   const [tmpList,setTmpList]=useState({name:'',dataPos:0})
-  const [showSMode,setShowSMode]=useState(true)
+  const [showSMode,setShowSMode]=useState(false)
+  const [showCrMo,setShowCrMo]=useState(true)
+  // {name:'Nome1',image:require('./../../assets/icon.png'),imageSet:1}
 
-
+  // Cria elementos da tela
   // Janela de configuração de item da lista
   function selectResizeMode(){
     const [tmp,setTmp]=useState('')
@@ -95,7 +97,81 @@ export default function App() {
       )
     }
   }
+  // Lista de itens
+  function showItemList(){
+    itemLista.map((i,index)=>(
+      <View key={index} style={styles.itemList}>
+          <TouchableOpacity activeOpacity={0.8}>
+              <Image style={[styles.itemImg,{resizeMode:imageSet[i.imageSet]}]} source={i.image}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.itemGear} onPress={
+            ()=>{
+              setTmpList({name:i.name,dataPos:index});
+              (showSMode)? setShowSMode(false):setShowSMode(true);
+            }
+          }>
+            <Icon name="gear" color="#fff" size={20}/>
+          </TouchableOpacity>
+          <Text style={styles.itemText}>{i.name}</Text>
+      </View>
+    ))
+  }
+  // Exibi uma janela para criar e configurar uma lista
+  function createItemList(){
+    const [tmp,setTmp]=useState('')
+    const [show,setShow]=useState(false)
 
+    if(showCrMo){
+    return(
+     <View style={styles.showSetItem}>
+              <ScrollView>
+                {/*Configurador do nome da Lista*/}
+                <View>
+                  <View style={styles.setImgHead}>
+                    <Text style={styles.setImgHeadTxt}>NOME</Text>
+                  </View>
+                  <View style={styles.setImgDesc}>
+                    <Text>Escreva o nome da lista</Text>
+                  </View>
+                  <View style={styles.setNameView}>
+                  <TextInput style={styles.setNameInput} value={tmp} onChangeText={tmp => setTmp(tmp)} maxLength={10} multiline={false}/>
+                  </View>
+                </View>
+                {/*Configura o tipo de lista*/}
+                <View>
+                  <View style={styles.setImgHead}>
+                    <Text style={styles.setImgHeadTxt}>TIPO</Text>
+                  </View>
+                  <View style={styles.setImgDesc}>
+                    <Text>Qual é o tipo de lista?</Text>
+                  </View>
+                  <View style={styles.setNameView}>
+                  <TextInput style={styles.setNameInput} value={tmp} onChangeText={tmp => setTmp(tmp)} maxLength={10} multiline={false}/>
+                  </View>
+                </View>
+                {/*Configurador do nome da Lista*/}
+                <View>
+                  <View style={styles.setImgHead}>
+                    <Text style={styles.setImgHeadTxt}>IMAGEM</Text>
+                  </View>
+                  <View style={styles.setImgDesc}>
+                    <Text>Selecione uma capa da sua lista</Text>
+                  </View>
+                  <View style={styles.setNameView}>
+                  <TextInput style={styles.setNameInput} multiline={false}/>
+                  <TouchableOpacity style={[styles.setNameBtn,{backgroundColor:'#a0f'}]} onPress={()=>setName(tmp)}>
+                    <Icon name="gear" color="#fff" size={20}/>
+                  </TouchableOpacity>
+                  </View>
+                
+                
+              </View>
+            </ScrollView>
+          </View>
+    )}
+  }
+
+  // funçãos de configuração
   // Configurar item da lista
   function setCapa(i){ 
     if(i!=itemLista[tmpList.dataPos].imageSet){
@@ -116,10 +192,10 @@ export default function App() {
   // Carrega os dados
   async function loadData(){
     try{      
-        let i=await  AsyncStorage.getItem('iList')
+        let i=await  AsyncStorage.getItem('i_List')
         let j=JSON.parse(i)       
+        if (j==null) j=[]
         setItemLista(j)
-    
     }catch(error){
       console.log('Erro ao Obter dados');
     }
@@ -127,7 +203,7 @@ export default function App() {
   // Salva os dados
   async function saveData(n=itemLista){
         try {
-            await AsyncStorage.setItem('iList',JSON.stringify(n))
+            await AsyncStorage.setItem('i_List',JSON.stringify(n))
             console.log('====================================');
             console.log('Dados Salvos');
             console.log('====================================');
@@ -135,6 +211,8 @@ export default function App() {
             console.log('Erro');
         }
   }
+
+  loadData()
 
   return (
     <View style={styles.background}>
@@ -147,30 +225,18 @@ export default function App() {
       <View style={styles.listItem}>
         {/*Itens com listas*/}
         {
-          itemLista.map((i,index)=>(
-            <View key={index} style={styles.itemList}>
-                <TouchableOpacity activeOpacity={0.8}>
-                    <Image style={[styles.itemImg,{resizeMode:imageSet[i.imageSet]}]} source={i.image}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.itemGear} onPress={
-                  ()=>{
-                    setTmpList({name:i.name,dataPos:index});
-                    (showSMode)? setShowSMode(false):setShowSMode(true);
-                  }
-                }>
-                  <Icon name="gear" color="#fff" size={20}/>
-                </TouchableOpacity>
-                <Text style={styles.itemText}>{i.name}</Text>
-            </View>
-          ))
+          showItemList()
         }
         {/*Buttão de adicinar à lista*/}
-        <TouchableOpacity activeOpacity={0.95} style={styles.addButton}>
+        <TouchableOpacity activeOpacity={0.55} style={styles.addButton}>
           <Text>+</Text>
         </TouchableOpacity>
       </View>
       {
         selectResizeMode()
+      }
+      {
+        createItemList()
       }
     </View>
   );
