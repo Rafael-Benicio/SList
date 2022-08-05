d0ad5b2ef67b4b5a506eed103e242604c663e1c4
Rafael-Benicio <jerobenicio@gmail.com>
2022-08-05 14:50:23 -0300

    Bug fix

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,9 +8,7 @@ import globals from "./../globals"
 import TextAviso from "./../components/TextAviso"
 
 import * as ImagePicker from 'expo-image-picker';
-
 import * as FileSystem from 'expo-file-system';
-import * as Permissions from 'expo-permissions'
 import * as MediaLibrary from 'expo-media-library'
 import { StorageAccessFramework } from 'expo-file-system';
 
