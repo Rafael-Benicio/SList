fc993343c85869441edf0b1a289319f239a4cb06
Rafael-Benicio <jerobenicio@gmail.com>
2024-02-12 18:27:40 -0300

    add : arquivo de tema

---
+++ src/utils/themePalet.js
@@ -0,0 +1,27 @@
+const theme = {
+  base: "#a0f",
+  lightBase: "#b6f",
+  headerText: "#fff",
+  windowBackground: "#fff",
+  innerInputText: "#ddd",
+  // Group
+  textCoverGroupColor: "#fff",
+  groupCoverColorLayer: "#000",
+  textBtnGroup: "#fff",
+  textDeleteGroupBtn: "#fff",
+  // Buttons
+  lightNegate: "#ff7774",
+  lightOkay: "#ccffc4",
+  okay: "#0f0",
+  negate: "#f00",
+  darkOkay: "#0a0",
+  darkNegate: "#a00",
+  // List
+  itemView: "#dddd",
+  itemViewValues: "#eeeeee",
+  textDescColor: "#000",
+  // Icon
+  iconColor: "#fff",
+};
+
+export default theme;
